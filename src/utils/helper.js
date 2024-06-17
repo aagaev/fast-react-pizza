717e3da6ac0b4d87f250c4617aa53eb12c3122dd
aagaev <agaeana@gmail.com>
2024-06-17 23:17:18 +0400

    fix: update format date, currencym calcTime functions

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,86 +1,22 @@
-// Test ID: IIDSAT
-
-import {
-  calcMinutesLeft,
-  formatCurrency,
-  formatDate,
-} from "../../utils/helpers";
-
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
-function Order() {
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order;
-  const deliveryIn = calcMinutesLeft(estimatedDelivery);
-
-  return (
-    <div>
-      <div>
-        <h2>Status</h2>
-
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
-        </div>
-      </div>
-
-      <div>
-        <p>
-          {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
-        </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
-      </div>
-
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
-      </div>
-    </div>
-  );
-}
-
-export default Order;
+export function formatCurrency(value) {
+    return new Intl.NumberFormat("en", {
+      style: "currency",
+      currency: "EUR",
+    }).format(value);
+  }
+  
+  export function formatDate(dateStr) {
+    return new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "short",
+      hour: "2-digit",
+      minute: "2-digit",
+    }).format(new Date(dateStr));
+  }
+  
+  export function calcMinutesLeft(dateStr) {
+    const d1 = new Date().getTime();
+    const d2 = new Date(dateStr).getTime();
+    return Math.round((d2 - d1) / 60000);
+  }
+  
