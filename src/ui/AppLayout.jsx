29db3bbea6f86841f14f85603c7d5990e1628a3b
aagaev <agaeana@gmail.com>
2024-06-19 15:24:32 +0400

    feat: create loader & apply css styles in AppLayout.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,24 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 function AppLayout() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+  console.log(isLoading);
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
